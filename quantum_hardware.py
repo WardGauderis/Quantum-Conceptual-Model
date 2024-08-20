c2bac0faea6e7bc1bdc0106a49631388e03c3db9
Ward Gauderis <ward.gauderis@vub.be>
2024-08-21 01:07:16 +0200

    ready for quantum

--- quantum_hardware.py
+++ quantum_hardware.py
@@ -1,16 +1,21 @@
 # %%
 
 from random import randint
+
+import numpy as np
 import pennylane as qml
 import torch as t
 from einops import rearrange, reduce, repeat
+from qiskit.result import marginal_counts
+from qiskit.visualization import plot_distribution
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit_ibm_runtime.fake_provider.fake_provider import FakeKyiv
 
 from architecture import Hybrid
 from architecture.quantum import VQC
 from data_modules import EntangledConceptDataModule
 from utils.circuits import create_circuit
-
-import numpy as np
+import matplotlib.pyplot as plt
 
 # %% BLACKBIRD DATASET AND MODELS
 
@@ -74,7 +79,12 @@ probs = t.stack([row_probs, column_probs], dim=1)
 probs = reduce(probs, "puzzle orientation -> puzzle", "prod")
 
 
+# TODO: plot instead
 def print_stats(probs, labels):
+    plt.hist(probs.cpu().detach().flatten(), bins=50)
+    plt.xlabel('Property Probability')
+    plt.show()
+    
     print("Positive")
     print(f"min: {probs[labels == 1].min()}", end=" ")
     print(f"mean: {probs[labels == 1].mean()}", end=" ")
@@ -101,13 +111,12 @@ print(f"Blackbird detection accuracy: {accuracy}")
 properties = blackbird_model.config.properties
 offsets = blackbird_model.config.offsets
 
-property_encodings = blackbird_model.vqc.concept_weights.weight
+property_encodings = blackbird_model.vqc.concept_weights.weight.detach()
 color_encodings = property_encodings[offsets[0] : offsets[0] + len(properties[0])]
 position_encodings = property_encodings[offsets[1] : offsets[1] + len(properties[1])]
 
 # %% COLOR AND POSITION CIRCUITS
 
-dev = qml.device("default.qubit", wires=10)
 
 # influences position circuit and selects color constraint
 missing_row = randint(0, 2)
@@ -116,8 +125,12 @@ missing_column = randint(0, 2)
 
 print(f"Missing panel at ({missing_row}, {missing_column})")
 
+# %%  COLOR AND POSITION PREDICTIONS
 
-def create_and_visualise_circuit(vqc: VQC, row_wise: bool) -> qml.QNode:
+
+def create_and_visualise_circuit(
+    vqc: VQC, dev: qml.Device, row_wise: bool
+) -> qml.QNode:
     circuit = qml.QNode(
         create_circuit(
             "generative",
@@ -131,7 +144,7 @@ def create_and_visualise_circuit(vqc: VQC, row_wise: bool) -> qml.QNode:
 
     instance = t.zeros(vqc.config.num_instance_domains, 3)
     concept = rearrange(
-        vqc.concept_weights.weight,
+        vqc.concept_weights.weight.detach(),
         "none (layer domain weights) -> (none layer) domain weights",
         layer=vqc.config.layers,
         weights=3,
@@ -145,30 +158,28 @@ def create_and_visualise_circuit(vqc: VQC, row_wise: bool) -> qml.QNode:
     return circuit
 
 
-color_circuit = create_and_visualise_circuit(distribute_three, row_wise=True)
-position_circuit = create_and_visualise_circuit(progression, row_wise=False)
-
-# %%  COLOR AND POSITION PREDICTIONS
-
-
 def make_property_predictions(
-    vqc: VQC, circuit: qml.QNode, property_encodings: t.Tensor, row_wise: bool
-) -> t.Tensor:
+    vqc: VQC,
+    circuit: qml.QNode,
+    instance_encodings: t.Tensor,
+    property_encodings: t.Tensor,
+    row_wise: bool,
+) -> tuple[t.Tensor, t.Tensor]:
     if row_wise:
         row = repeat(
-            encodings[labels == 1, missing_row, :],
+            instance_encodings[:, missing_row, :],
             "puzzle column domain encoding -> (column domain) encoding (puzzle property)",
             property=len(property_encodings),
         )
     else:  # if column-wise, replace row with column
         row = repeat(
-            encodings[labels == 1, :, missing_column],
+            instance_encodings[:, :, missing_column],
             "puzzle row domain encoding -> (row domain) encoding (puzzle property)",
             property=len(property_encodings),
         )
 
     concept = rearrange(
-        vqc.concept_weights.weight,
+        vqc.concept_weights.weight.detach(),
         "none (layer domain weights) -> (none layer) domain weights",
         layer=vqc.config.layers,
         weights=3,
@@ -180,19 +191,24 @@ def make_property_predictions(
     properties = repeat(
         conjugated_properties,
         "property (encoding) -> domain encoding (puzzle property)",
-        puzzle=len(encodings[labels == 1]),
+        puzzle=len(instance_encodings),
         domain=1,
     )
 
-    property_probs = circuit(row, concept, properties)[:, 0]
+    # print(row.shape, concept.shape, properties.shape)
+
+    full_probs = circuit(row, concept, properties)
     property_probs = rearrange(
-        property_probs,
+        full_probs[:, 0],
         "(puzzle property) -> puzzle property",
         property=len(property_encodings),
     )
 
+    plt.hist(property_probs.cpu().detach().flatten(), bins=50)
+    plt.show()
+
     property_preds = t.argmax(property_probs, dim=1)
-    return property_preds
+    return property_preds, full_probs
 
 
 def check_color(colors: np.ndarray):
@@ -217,35 +233,97 @@ def check_puzzle(puzzle: np.ndarray):
     return color_accuracy and position_accuracy
 
 
+def check_predictions(color_preds, position_preds, property_labels):
+    preds = property_labels.copy()
+    preds[:, :, missing_row, missing_column] = "MISSING"
+
+    preds[:, 0, missing_row, missing_column] = properties[0, color_preds.cpu()]
+    preds[:, 1, missing_row, missing_column] = properties[1, position_preds.cpu()]
+
+    correct = np.apply_along_axis(
+        lambda puzzle: check_puzzle(
+            rearrange(
+                puzzle, "(property row column) -> property row column", row=3, column=3
+            )
+        ),
+        1,
+        rearrange(preds, "puzzle property row column -> puzzle (property row column)"),
+    )
+
+    print(f"Blackbird solving accuracy: {correct.mean()} ({len(correct)} puzzles)")
+
+    new_solution = property_labels[correct] != preds[correct]
+    print(
+        f"Found {new_solution.sum()} new solutions out of {len(new_solution)} solved puzzles"
+    )
+
+
 # %%
 
-color_preds = make_property_predictions(
-    distribute_three, color_circuit, color_encodings, row_wise=True
+dev = qml.device("default.qubit", wires=10)
+
+color_circuit = create_and_visualise_circuit(distribute_three, dev, row_wise=True)
+position_circuit = create_and_visualise_circuit(progression, dev, row_wise=False)
+
+color_preds, full_color_probs = make_property_predictions(
+    distribute_three,
+    color_circuit,
+    encodings[labels == 1],
+    color_encodings,
+    row_wise=True,
 )
-position_preds = make_property_predictions(
-    progression, position_circuit, position_encodings, row_wise=False
-)
-
-
-preds = property_labels.copy()
-preds[:, :, missing_row, missing_column] = "MISSING"
-
-preds[:, 0, missing_row, missing_column] = properties[0, color_preds.cpu()]
-preds[:, 1, missing_row, missing_column] = properties[1, position_preds.cpu()]
-
-
-correct = np.apply_along_axis(
-    lambda puzzle: check_puzzle(
-        rearrange(
-            puzzle, "(property row column) -> property row column", row=3, column=3
-        )
-    ),
-    1,
-    rearrange(preds, "puzzle property row column -> puzzle (property row column)"),
+position_preds, full_position_probs = make_property_predictions(
+    progression,
+    position_circuit,
+    encodings[labels == 1],
+    position_encodings,
+    row_wise=False,
 )
 
-print(f"Blackbird solving accuracy: {correct.mean()}")
+check_predictions(color_preds, position_preds, property_labels)
 
-new_solution = property_labels != preds
-print(f"Found {new_solution.sum()} new solutions out of {len(new_solution)} solvable puzzles")
+# %% ############################################################################
+
+# service = QiskitRuntimeService(channel="ibm_quantum", instance="ibm-q/open/main")
+# backend = service.least_busy(operational=True, simulator=False, min_num_qubits=10)
+# print(f"{backend.name}: {backend.status().pending_jobs} pending jobs")
+
+backend = FakeKyiv()
+print(f"{backend.name}: {backend.status().pending_jobs} pending jobs")
+
+dev = qml.device("qiskit.remote", wires=10, backend=backend, shots=2**12, optimization_level=3)
+
+# job = service.jobs()[0]
+# result = job.result()
+
+# plot_distribution(result.get_counts(0), number_to_keep=20)
+
+# marginal = marginal_counts(result, [0])
+# plot_distribution(marginal.get_counts(0), number_to_keep=20)
+
 # %%
+%%time
+
+dev = qml.device("default.qubit", wires=10)
+
+color_circuit = create_and_visualise_circuit(distribute_three, dev, row_wise=True)
+position_circuit = create_and_visualise_circuit(progression, dev, row_wise=False)
+
+amount = len(labels[labels == 1])
+
+color_preds, full_color_probs = make_property_predictions(
+    distribute_three,
+    color_circuit,
+    encodings[labels == 1][:amount],
+    color_encodings,
+    row_wise=True,
+)
+position_preds, full_position_probs = make_property_predictions(
+    progression,
+    position_circuit,
+    encodings[labels == 1][:amount],
+    position_encodings,
+    row_wise=False,
+)
+
+check_predictions(color_preds, position_preds, property_labels[:amount])
